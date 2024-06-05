b7a28dc2a2a611cac37c8a17af318faaaa35ffd9
c-rahtz <c.rahtz@gmail.com>
2024-06-05 15:58:50 -0400

    filling out top-level pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,4 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    // vite: {
-    //     css: {
-    //         preprocessorOptions: {
-    //             scss: {
-    //                 additionalData: `@import "styles/variables.scss";`
-    //             }
-    //         }
-    //     }
-    // }
 });
